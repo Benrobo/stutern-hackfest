da33dde1aaa65b6522e3fef04354911f4f5453ee
Benrobo <alumonabenaiah71@gmail.com>
2023-11-20 13:38:44 +0100

    updated

--- packages/app/src/pages/chatbots.tsx
+++ packages/app/src/pages/chatbots.tsx
@@ -1,22 +1,35 @@
-import { FlexColCenter, FlexColStart, FlexRowCenterBtw, FlexRowStart, FlexRowStartBtw } from '@/components/Flex'
-import Layout from '@/components/Layout'
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { cn } from '@/lib/utils';
-import { FileText, Theater } from 'lucide-react';
-import React, { useState } from 'react'
+import {
+  FlexColCenter,
+  FlexColStart,
+  FlexRowCenterBtw,
+  FlexRowStart,
+  FlexRowStartBtw,
+} from "@/components/Flex";
+import Layout from "@/components/Layout";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { FileText, Theater } from "lucide-react";
+import React, { useState } from "react";
 
 const Tabs = ["Widget", "Training Data"] as const;
 
-
 function Chat() {
-const [activeTab, setActiveTab] =
-  useState<(typeof Tabs)[number]>("Widget");
-  
+  const [activeTab, setActiveTab] = useState<(typeof Tabs)[number]>("Widget");
 
   return (
     <Layout activePage="chatbots">
       <FlexColStart className="w-full px-5 py-5">
+        <FlexRowCenterBtw className="w-full">
+          <h1 className="text-white-100 font-ppSB">Create chatbot</h1>
+          <Button
+            variant={"primary"}
+            className=" text-white-100 font-ppSB text-[13px]"
+          >
+            Create Bot
+          </Button>
+        </FlexRowCenterBtw>
+
         <FlexRowStart className="w-fit mt-9 border-b-solid border-b-[1px] border-b-white-600 gap-0 ">
           {Tabs.map((t) => (
             <Button
@@ -46,29 +59,77 @@ const [activeTab, setActiveTab] =
         </FlexRowStart>
 
         {activeTab === "Widget" && <ChatWidget />}
-          
+        {activeTab === "Training Data" && <ChatTrainingData />}
       </FlexColStart>
     </Layout>
   );
 }
 
-export default Chat
+export default Chat;
 
-
-function ChatWidget(){
+function ChatWidget() {
   return (
-    <FlexRowStartBtw className='w-full h-full'>
-      <FlexColStart className='w-full h-full'>
-        <Input placeholder='Name' />
-        <Input placeholder='Agent Name' />
+    <FlexRowStartBtw className="w-full h-full">
+      <FlexColStart className="w-full h-full">
+        <Input placeholder="Name" />
+        <Input placeholder="Agent Name" />
       </FlexColStart>
-      <FlexColStart className='w-fit h-full min-h-[400px] min-w-[400px] bg-blue-200 '>
+      <FlexColStart className="w-fit h-full min-h-[400px] min-w-[400px] bg-blue-200 ">
         output comp
       </FlexColStart>
     </FlexRowStartBtw>
-  )
+  );
 }
 
+function ChatTrainingData() {
+  const [selectedDataType, setSelectedDataType] = useState<"file" | "webpage">(
+    "file"
+  );
+  const [links, setLinks] = useState([])
+
+  return (
+    <FlexColStart className="w-full h-full">
+      <p className="text-white-100">Training Data</p>
+      {/* Tags to differentiate choosen selected data type */}
+
+      <FlexRowStart className="w-full h-fit">
+        <Button
+          variant="primary"
+          className="text-white-100 text-[8px] scale-[.95] font-ppSB"
+          disabled
+        >
+          Upload File
+        </Button>
+        <Button
+          variant="primary"
+          className="text-white-100 text-[8px] scale-[.95] font-ppSB"
+        >
+          Webpage
+        </Button>
+      </FlexRowStart>
+
+      {/* Input field to enter webpage url */}
+      <FlexRowStart className="w-full mt-3">
+        <Input placeholder="Enter webpage url" type="url" className="w-auto min-w-[300px] bg-dark-200 border-none text-white-100 placeholder:text-white-300 text-[12px] font-ppR" />
+        <Button
+          variant="primary"
+          className="text-white-100 text-[8px] scale-[.95] font-ppSB"
+        >
+          Fetch Links
+        </Button>
+      </FlexRowStart>
+
+      {/* fetched urls */}
+      <FlexColStart className="w-full">
+        <li className="">
+          <a href="#" className="text-white-300 text-[12px] font-ppSB">
+            https://www.example.com
+          </a>
+        </li>
+      </FlexColStart>
+    </FlexColStart>
+  );
+}
 
 function renderBaseTabIcon(
   tab: (typeof Tabs)[number],

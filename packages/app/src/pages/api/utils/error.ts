587a000f0df88aebee7fc56a13850c0a06330114
Benrobo <alumonabenaiah71@gmail.com>
2023-11-21 01:25:44 +0100

    Fix error handling in catchErrors function

--- packages/app/src/pages/api/utils/error.ts
+++ packages/app/src/pages/api/utils/error.ts
@@ -2,13 +2,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 import HttpException from "../exception";
+import { RESPONSE_CODE } from "@/types";
 
 export default function catchErrors(fn: Function) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         await fn(req, res)
     } catch (e: any) {
-        console.log(e?.message)
+         const code = RESPONSE_CODE[RESPONSE_CODE.INTERNAL_SERVER_ERROR];
+         console.log(`😥 Error [${code}]: ${e?.message}`);
         if(e instanceof HttpException){
           return res.status(e.status).json({
             code: e.code,
@@ -18,7 +20,7 @@ export default function catchErrors(fn: Function) {
           })
         }
         res.status(500).json({
-          code: "SERVER_ERROR",
+          code: code,
           status: 500,
           message: `Internal server error`,
           error: e

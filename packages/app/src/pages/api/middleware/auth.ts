01a7cf44f5f6d04bc649783c33a4892a1afe6553
Benrobo <alumonabenaiah71@gmail.com>
2023-11-21 02:51:27 +0100

    Fix unauthorized user error and add user existence check

--- packages/app/src/pages/api/middleware/auth.ts
+++ packages/app/src/pages/api/middleware/auth.ts
@@ -1,13 +1,27 @@
 import { getAuth } from "@clerk/nextjs/server";
 import HttpException from "../exception";
 import { NextApiRequest, NextApiResponse } from "next";
+import { RESPONSE_CODE } from "@/types";
+import prisma from "../config/prisma";
 
 export function isAuthenticated(fn: Function) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     const currUser = getAuth(req);
     if (!currUser.userId) {
-      throw new HttpException("Unauthorized user", "UNAUTHORIZED", 401);
+      throw new HttpException("Unauthorized user", RESPONSE_CODE.UNAUTHORIZED, 401);
     }
+
+    // check if user exists
+    const user  = await prisma.users.findUnique({
+        where: {
+            id: currUser.userId
+        }
+    });
+
+    if(!user){
+        throw new HttpException("Unauthorized, user not found", RESPONSE_CODE.UNAUTHORIZED, 401);
+    }
+
     (req as any)["user"] = { id: currUser.userId };
     await fn(req, res);
   };

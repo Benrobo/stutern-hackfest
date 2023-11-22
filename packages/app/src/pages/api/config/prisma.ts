a86c153d9e4f18276a890dfc10dc0da416b15790
Benrobo <alumonabenaiah71@gmail.com>
2023-11-22 03:58:07 +0100

    Refactor Prisma initialization logic

--- packages/app/src/pages/api/config/prisma.ts
+++ packages/app/src/pages/api/config/prisma.ts
@@ -1,5 +1,18 @@
+/** @type {PrismaClient} */
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+}
+// `stg` or `dev`
+else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+
+  prisma = (global as any).prisma as PrismaClient;
+}
 
 export default prisma;

45165854c6f54716ed862b53fa86c87416a02fbe
Benrobo <alumonabenaiah71@gmail.com>
2023-11-23 10:05:34 +0100

    Fix conversation retrieval and handle AI conversation

--- packages/app/src/pages/api/controllers/chat.controller.ts
+++ packages/app/src/pages/api/controllers/chat.controller.ts
@@ -1,7 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import HttpException from "../exception";
 import zodValidation from "../utils/zodValidator";
-import { chatConversationSchema, createChatSchema } from "../utils/validation";
+import {
+  anonymousConversationSchema,
+  assistantConersationSchema,
+  createChatSchema,
+} from "../utils/validation";
 import { RESPONSE_CODE } from "@/types";
 import redisClient from "../config/redis";
 import { extractLinksFromWebPages } from "../utils";
@@ -25,6 +29,17 @@ type ConversationPayload = {
   chatId: string;
   anonymous_id: string;
   sender_type: "ANONYMOUS" | "AI" | "ADMIN";
+};
+
+type AnonymousConvPayload = {
+  message: string;
+  chatId: string;
+  anonymous_id: string;
+};
+
+type AsistantConvPayload = {
+  anonymous_message: string;
+  conversation_id: string;
 };
 
 type SimilaritiesResult = {
@@ -204,14 +219,51 @@ export default class ChatController {
 
   async getUserConversations() {}
 
-  async getAnonymousConversations() {}
+  async getAnonymousConversations(req: NextApiRequest, res: NextApiResponse) {
+    const { conversation_id } = req.query;
 
-  // conversations are mean't to be created/started only from anonymous users
-  async chatConversation(req: NextApiRequest, res: NextApiResponse) {
-    const payload = req.body as ConversationPayload;
+    // check if conversation exists
 
-    // validate
-    const _validated = await zodValidation(chatConversationSchema, req, res);
+    const _conversation = await prisma.conversations.findFirst({
+      where: {
+        id: conversation_id as string,
+      },
+    });
+
+    if (!_conversation) {
+      throw new HttpException(
+        "Conversation not found",
+        RESPONSE_CODE.CONVERSATION_NOT_FOUND,
+        404
+      );
+    }
+
+    const conversations = await prisma.messages.findMany({
+      where: {
+        conversation_id: conversation_id as string,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return sendResponse.success(
+      res,
+      RESPONSE_CODE.SUCCESS,
+      "Conversations retrieved successfully",
+      200,
+      conversations
+    );
+  }
+
+  async anonymousConversation(req: NextApiRequest, res: NextApiResponse) {
+    const payload = req.body as AnonymousConvPayload;
+    const _validated = await zodValidation(
+      anonymousConversationSchema,
+      req,
+      res
+    );
+
     if (!_validated) {
       throw new HttpException(
         "Invalid conversation payload",
@@ -221,7 +273,6 @@ export default class ChatController {
     }
 
     const { message, chatId, anonymous_id } = payload;
-
     // check if chat exists
     const chat = await prisma.chats.findFirst({
       where: { id: chatId },
@@ -261,19 +312,104 @@ export default class ChatController {
       });
     }
 
-    // ADMIN
-    if ((req as any).user?.id) {
-      // handle admin conversation
-    } else {
-      // handle anonymous conversation
+    // store message
+    await prisma.messages.create({
+      data: {
+        message,
+        sender_type: "ANONYMOUS",
+        conversation_id,
+      },
+    });
 
+    // send conversation_id and anonymous_id to client
+    return sendResponse.success(
+      res,
+      RESPONSE_CODE.SUCCESS,
+      "Message sent successfully",
+      200,
+      {
+        conversation_id,
+        anonymous_id: _anonymous_id,
+        message,
+      }
+    );
+  }
+
+  async assistantConversation(req: NextApiRequest, res: NextApiResponse) {
+    const payload = req.body as AsistantConvPayload;
+    const _validated = await zodValidation(
+      assistantConersationSchema,
+      req,
+      res
+    );
+
+    if (!_validated) {
+      throw new HttpException(
+        "Invalid conversation payload",
+        RESPONSE_CODE.VALIDATION_ERROR,
+        400
+      );
+    }
+
+    const { conversation_id } = payload;
+
+    // check if conversation exists
+    const _conversation = await prisma.conversations.findFirst({
+      where: { id: conversation_id },
+      include: {
+        chat: true,
+      },
+    });
+
+    if (!_conversation) {
+      throw new HttpException(
+        "Conversation not found",
+        RESPONSE_CODE.CONVERSATION_NOT_FOUND,
+        404
+      );
+    }
+
+    const lastAnonymousMessage = await prisma.messages.findFirst({
+      where: {
+        conversation_id: conversation_id as string,
+        sender_type: "ANONYMOUS",
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const anonymous_message = lastAnonymousMessage?.message ?? "";
+
+    // check if conversation isn't in control by ADMIN
+    if (_conversation.in_control === "User") {
+      // ai won't be able to respond, a message saying "please wait for an agent to respond" will be sent
+      await prisma.messages.create({
+        data: {
+          message: "Please wait for an agent to respond",
+          sender_type: "AI",
+          conversation_id,
+        },
+      });
+
+      return sendResponse.success(
+        res,
+        RESPONSE_CODE.SUCCESS,
+        "Message sent successfully",
+        200,
+        {
+          aiResponse: "Please wait for an agent to respond",
+        }
+      );
+    } else {
       const tmpCache: Record<string, any> = {};
       let embeddings: number[] = [];
 
-      if (tmpCache[message]) {
-        embeddings = tmpCache[message];
+      if (tmpCache[anonymous_message]) {
+        embeddings = tmpCache[anonymous_message];
       } else {
-        const resp = await aiServices.retrieveEmbeddings(message, {});
+        const resp = await aiServices.retrieveEmbeddings(anonymous_message, {});
+        console.log({ resp });
         embeddings = resp[0].embedding;
       }
 
@@ -298,7 +434,7 @@ export default class ChatController {
         });
       }
 
-      const highest_similarity_search = _similarities.slice(0, 2);
+      const highest_similarity_search = _similarities.slice(0, 5);
 
       // combined highest content with newline
       const combinedContent = highest_similarity_search
@@ -306,37 +442,27 @@ export default class ChatController {
         .join("\n");
 
       const _templates = await aiServices.createPromptTemplate(
-        message,
-        chat.agent_name,
+        anonymous_message,
+        _conversation.chat.agent_name,
         combinedContent,
-        chat.name
+        _conversation.chat.name
       );
 
       const aiResponse = await aiServices._webChatComplition(
         _templates[0]?.content as string,
-        message
+        anonymous_message
       );
 
       // get metadata content or url if exists
-      const metadata = highest_similarity_search.map((sim) => sim.metadata).filter(s => s.url);
-      
+      const metadata = highest_similarity_search
+        .map((sim) => sim.metadata)
+        .filter((s) => s.url);
+
       const _metadata = metadata.length > 0 ? metadata[0].url : null;
-
-      // create messages (anonymous, ai)
-      // create anonymous conversation first
-      // check if airesponse exists, else add a custom error message
-
-      const _anonymousMsgCreated =  await prisma.messages.create({
-        data: {
-          message,
-          sender_type: "ANONYMOUS",
-          conversation_id,
-        },
-      });
 
       let aiResponseMsgCreated;
 
-      if(aiResponse && aiResponse.length > 0){
+      if (aiResponse && aiResponse.length > 0) {
         aiResponseMsgCreated = await prisma.messages.create({
           data: {
             message: aiResponse as string,
@@ -345,7 +471,7 @@ export default class ChatController {
             metadata: _metadata,
           },
         });
-      }else{
+      } else {
         aiResponseMsgCreated = await prisma.messages.create({
           data: {
             message: "Sorry, something went wrong. Please try again later.",
@@ -361,7 +487,7 @@ export default class ChatController {
         "Message sent successfully",
         200,
         {
-          anonymous: _anonymousMsgCreated,
+          anonymous: anonymous_message,
           aiResponse: aiResponseMsgCreated,
         }
       );

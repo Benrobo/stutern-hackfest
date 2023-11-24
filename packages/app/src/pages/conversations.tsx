bc3db662de31181388450fef46454f00213fdec4
Benrobo <alumonabenaiah71@gmail.com>
2023-11-24 18:59:03 +0100

    Fix bug in login functionality

--- packages/app/src/pages/conversations.tsx
+++ packages/app/src/pages/conversations.tsx
@@ -13,6 +13,7 @@ import Modal from "@/components/Modal";
 import { Spinner } from "@/components/Spinner";
 import { Input } from "@/components/ui/input";
 import {
+  adminReplyToConversation,
   getAllChats,
   getConversationMessages,
   getConversations,
@@ -21,13 +22,14 @@ import {
 import { cn } from "@/lib/utils";
 import { ResponseData } from "@/types";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { Bot, SendHorizonal, User2 } from "lucide-react";
+import { Bot, RefreshCcw, RefreshCw, SendHorizonal, User2 } from "lucide-react";
 import React, { useEffect } from "react";
 import toast from "react-hot-toast";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import MarkdownRenderer from "@/components/MarkdownRender";
 import { useSelectedLayoutSegments } from "next/navigation";
+import Image from "next/image";
 dayjs.extend(relativeTime);
 
 type ConversationsData = {
@@ -51,7 +53,7 @@ type ConversationMessages = {
   id: string;
   userId: string | null;
   message: string;
-  sender_type: "ANONYMOUS" | "USER" | "AI";
+  sender_type: "ANONYMOUS" | "ADMIN" | "AI";
   conversation_id: string;
   createdAt: string;
   user: null | {
@@ -82,6 +84,7 @@ function Conversations() {
   const [selectedChat, setSelectedChat] = React.useState<ChatDetails | null>(
     null
   );
+  const [adminReply, setAdminReply] = React.useState("");
   const getChatQuery = useQuery({
     queryFn: async () => await getAllChats(),
     queryKey: ["getAllChats"],
@@ -94,6 +97,9 @@ function Conversations() {
   });
   const takeConvControlMut = useMutation({
     mutationFn: async (conv_id: string) => takeControl(conv_id),
+  });
+  const replyToConvMut = useMutation({
+    mutationFn: async (data: any) => adminReplyToConversation(data),
   });
 
   const messageList = React.useRef(null);
@@ -137,6 +143,7 @@ function Conversations() {
       const data = getConvMessagesMutation.data as ResponseData;
       const messages = data.data;
       setMessages(messages);
+      scrollToBottom();
     }
   }, [
     getConvMessagesMutation.data,
@@ -160,6 +167,26 @@ function Conversations() {
     takeConvControlMut.data,
     takeConvControlMut.isPending,
     takeConvControlMut.error,
+  ]);
+
+  // reply to conversation
+  useEffect(() => {
+    if (replyToConvMut.error) {
+      const data = (replyToConvMut.error as any)?.response
+        ?.data as ResponseData;
+      replyToConvMut.reset();
+      toast.error(data?.message as string);
+    }
+    if (replyToConvMut.data) {
+      // refetch conversations
+      replyToConvMut.reset()
+      getConvMessagesMutation.mutate(selectedConversation?.id as string);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    replyToConvMut.data,
+    replyToConvMut.isPending,
+    replyToConvMut.error,
   ]);
 
   useEffect(() => {
@@ -274,6 +301,15 @@ function Conversations() {
                 </p>
               </FlexColStart>
               <FlexRowCenterBtw>
+                <button onClick={()=> getConvMessagesMutation.mutate(selectedConversation?.id as string)}>
+                  <RefreshCw
+                    size={18}
+                    className={cn(
+                      "bg-dark-100 p-1 rounded-[50%] text-white-100",
+                      getConvMessagesMutation.isPending && "animate-spin"
+                    )}
+                  />
+                </button>
                 <button>
                   <User2
                     size={18}
@@ -303,39 +339,41 @@ function Conversations() {
             </FlexRowCenterBtw>
           </FlexColStart>
 
-          {getConvMessagesMutation.isPending ? (
-            <FlexColCenter className="w-full h-full">
-              <Spinner color="#000" size={15} />
+          {getConvMessagesMutation.isPending && (
+            <FlexColCenter className="w-auto h-auto absolute top-[5em] right-10 z-[999]">
+              <Spinner color="#000" size={20} />
             </FlexColCenter>
-          ) : (
-            <>
-              <FlexColStart
-                ref={messageList}
-                className="w-full h-full max-h-[550px] overflow-y-scroll hideScrollBar gap-5 px-4 py-4 scroll-smooth"
-              >
-                {messages.length > 0 ? (
-                  messages.map((msg) => {
-                    if (msg.sender_type === "ANONYMOUS") {
-                      return (
-                        <FlexColEnd key={msg.id} className="w-full gap-1">
-                          <FlexRowStartBtw className="w-fit">
-                            <FlexColStart className={"w-fit gap-1"}>
-                              <div className="text-white-100 text-[11px] font-ppSB w-fit max-w-[400px] px-4 py-2 h-auto rounded-lg bg-blue-100 leading-[15px] whitespace-wrap swissai-chat-msg">
-                                <MarkdownRenderer content={msg.message} />
-                              </div>
-                              <p className="text-white-400 font-ppR leading-none text-[10px]">
-                                {dayjs(msg.createdAt).fromNow()}
-                              </p>
-                            </FlexColStart>
-                            <User2
-                              size={25}
-                              className="bg-white-400/20 p-1 rounded-[50%] text-white-100"
-                            />
-                          </FlexRowStartBtw>
-                        </FlexColEnd>
-                      );
-                    } else {
-                      return (
+          )}
+
+          <FlexColStart
+            ref={messageList}
+            className="w-full h-full max-h-[550px] overflow-y-scroll hideScrollBar gap-5 px-4 py-4 scroll-smooth"
+          >
+            {messages.length > 0 ? (
+              messages.map((msg) => {
+                if (msg.sender_type === "ANONYMOUS") {
+                  return (
+                    <FlexColEnd key={msg.id} className="w-full gap-1">
+                      <FlexRowStartBtw className="w-fit">
+                        <FlexColStart className={"w-fit gap-1"}>
+                          <div className="text-white-100 text-[11px] font-ppSB w-fit max-w-[400px] px-4 py-2 h-auto rounded-lg bg-blue-100 leading-[15px] whitespace-wrap swissai-chat-msg">
+                            <MarkdownRenderer content={msg.message} />
+                          </div>
+                          <p className="text-white-400 font-ppR leading-none text-[10px]">
+                            {dayjs(msg.createdAt).fromNow()}
+                          </p>
+                        </FlexColStart>
+                        <User2
+                          size={25}
+                          className="bg-white-400/20 p-1 rounded-[50%] text-white-100"
+                        />
+                      </FlexRowStartBtw>
+                    </FlexColEnd>
+                  );
+                } else {
+                  return (
+                    <>
+                      {msg.sender_type === "AI" && (
                         <FlexColStart key={msg.id} className="w-full gap-1">
                           <FlexRowStartBtw className="w-fit">
                             <Bot
@@ -363,49 +401,86 @@ function Conversations() {
                             </FlexColStart>
                           </FlexRowStartBtw>
                         </FlexColStart>
-                      );
-                    }
-                  })
-                ) : (
-                  <FlexColCenter className="w-full h-full">
-                    <p className="text-dark-100 font-jbEB text-[12px] ">
-                      No messages
-                    </p>
-                  </FlexColCenter>
-                )}
-              </FlexColStart>
+                      )}
+                      {msg.sender_type === "ADMIN" && (
+                        <FlexColStart key={msg.id} className="w-full gap-1">
+                          <FlexRowStartBtw className="w-fit">
+                            <Image
+                              src={msg.user?.image as string}
+                              width={25}
+                              height={0}
+                              alt="user-image"
+                              className="rounded-[50%]"
+                            />
+                            <FlexColStart className={"w-fit gap-1"}>
+                              <div className="text-dark-200 text-[11px] font-ppSB w-fit max-w-[400px] px-4 py-2 h-auto rounded-lg bg-white-400/20 leading-[15px] whitespace-wrap swissai-chat-msg">
+                                <MarkdownRenderer content={msg.message} />
+                              </div>
+                              <p className="text-white-400 font-ppR leading-none text-[10px]">
+                                {dayjs(msg.createdAt).fromNow()}
+                              </p>
+                              <FlexRowStart className="w-full">
+                                {msg.metadata && (
+                                  <a
+                                    target="_blank"
+                                    href={msg.metadata}
+                                    className="text-dark-100 underline bg-white-400/20 px-2 py-1 text-[10px] rounded-md "
+                                  >
+                                    {msg.metadata}
+                                  </a>
+                                )}
+                              </FlexRowStart>
+                            </FlexColStart>
+                          </FlexRowStartBtw>
+                        </FlexColStart>
+                      )}
+                    </>
+                  );
+                }
+              })
+            ) : (
+              <FlexColCenter className="w-full h-full">
+                <p className="text-dark-100 font-jbEB text-[12px] ">
+                  No messages
+                </p>
+              </FlexColCenter>
+            )}
+          </FlexColStart>
 
-              <FlexColStart className="w-full bg-white-600 backdrop-blur bg-opacity-85 py-2 px-6 ">
-                <FlexRowCenter className="w-full px-4 border-solid border-[.5px] border-white-600 rounded-[30px] bg-white-100 shadow-md ">
-                  <Input
-                    className="w-full text-[12px] font-jbSB border-none outline-none rounded-none hover:border-none hover:outline-none focus-visible:ring-0 bg-transparent "
-                    autoFocus
-                    placeholder="Type a message..."
-                    // onChange={(e) => setUserMsg(e.target.value)}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter") {
-                        // addChatMessage(usermsg, "user");
-                        // setUserMsg("");
-                      }
-                    }}
-                    disabled={selectedConversation?.in_control === "AI"}
-                    // value={usermsg}
-                  />
-                  <button
-                    onClick={() => {
-                      // addChatMessage(usermsg, "user");
-                      // setUserMsg("");
-                    }}
-                  >
-                    <SendHorizonal
-                      size={30}
-                      className="p-[8px] text-dark-100 rounded-md"
-                    />
-                  </button>
-                </FlexRowCenter>
-              </FlexColStart>
-            </>
-          )}
+          <FlexColStart className="w-full bg-white-600 backdrop-blur bg-opacity-85 py-2 px-6 ">
+            <FlexRowCenter className="w-full px-4 border-solid border-[.5px] border-white-600 rounded-[30px] bg-white-100 shadow-md ">
+              <Input
+                className="w-full text-[12px] font-jbSB border-none outline-none rounded-none hover:border-none hover:outline-none focus-visible:ring-0 bg-transparent "
+                autoFocus
+                placeholder="Type a message..."
+                onChange={(e) => setAdminReply(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    replyToConvMut.mutate({
+                      conversation_id: selectedConversation?.id as string,
+                      message: adminReply,
+                    });
+                    setAdminReply("");
+                  }
+                }}
+                disabled={
+                  replyToConvMut.isPending || getConvMessagesMutation.isPending
+                }
+                value={adminReply}
+              />
+              <button
+                onClick={() => {
+                  // addChatMessage(usermsg, "user");
+                  // setUserMsg("");
+                }}
+              >
+                <SendHorizonal
+                  size={30}
+                  className="p-[8px] text-dark-100 rounded-md"
+                />
+              </button>
+            </FlexRowCenter>
+          </FlexColStart>
 
           {!selectedConversation && (
             <FlexColCenter className="w-full h-screen absolute top-0 left-0 bg-white-105">

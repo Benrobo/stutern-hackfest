3d08d011c9ce31b8d08e58a9bd8e657191d5e7a9
Benrobo <alumonabenaiah71@gmail.com>
2023-11-26 18:36:25 +0100

    Handle OPTIONS request in chat validation API

--- packages/app/src/pages/api/chat/valid/[chat_id].ts
+++ packages/app/src/pages/api/chat/valid/[chat_id].ts
@@ -4,6 +4,11 @@ import ChatController from "../../controllers/chat.controller";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const chatController = new ChatController();
+  // handle options request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
   if (req.method === "GET") {
     await chatController.isChatValid(req, res);
   }

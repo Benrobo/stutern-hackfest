19a8e11894960c0e084d2c11cfb2152f8912446a
Benrobo <alumonabenaiah71@gmail.com>
2023-11-27 00:00:55 +0100

    Update API URL

--- packages/app/src/lib/http/axios.ts
+++ packages/app/src/lib/http/axios.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://api.velozweb.dev";
+    : "https://swissai.vercel.app";
 
 // custom axios instance with necessary details
 const $axios = axios.create({
